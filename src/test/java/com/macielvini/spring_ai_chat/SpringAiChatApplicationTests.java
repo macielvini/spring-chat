1f04e2899f1759be1158dc209d2961ace2d2d91e
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-08 18:27:02 -0300

    chore: project init

---
+++ src/test/java/com/macielvini/spring_ai_chat/SpringAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macielvini.spring_ai_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
