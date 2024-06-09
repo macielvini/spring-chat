550515e8367a4d86ec6e87a2e2ba199711356bfa
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-08 21:33:53 -0300

    feat: add method to process message and send to user to controller

--- src/main/java/com/macielvini/spring_ai_chat/chat/ChatController.java
+++ src/main/java/com/macielvini/spring_ai_chat/chat/ChatController.java
@@ -2,6 +2,9 @@ package com.macielvini.spring_ai_chat.chat;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatMessageService chatMessageService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload ChatMessage message) {
+        ChatMessage savedMessage = chatMessageService.save(message);
+        messagingTemplate.convertAndSendToUser(message.getRecipientId(), "/queue/messages", ChatNotification.builder()
+                .chatId(savedMessage.getChatId())
+                .senderId(savedMessage.getSenderId())
+                .recipientId(savedMessage.getRecipientId())
+                .content(savedMessage.getContent())
+                .build());
+    }
 
     @GetMapping("/messages/{senderId}/{recipientId}")
     public ResponseEntity<List<ChatMessage>> findChatMessages(@PathVariable String senderId, @PathVariable String recipientId) {
