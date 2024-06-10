792a2c66660d97abe6a08670045abc8392c9ea92
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-10 15:18:32 -0300

    fix: user destination prefix

--- src/main/java/com/macielvini/spring_ai_chat/config/WebSocketConfig.java
+++ src/main/java/com/macielvini/spring_ai_chat/config/WebSocketConfig.java
@@ -17,12 +17,11 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/user");
         registry.setApplicationDestinationPrefixes("/app");
-        registry.setUserDestinationPrefix("/user");
+        registry.setUserDestinationPrefix("/user/{userId}/**");
     }
 
     @Override
